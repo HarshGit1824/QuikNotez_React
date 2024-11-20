976ad5a334174e2c1644615bc9ffaba225b4a47d
HarshalT18 <harshalthaware9049@gmail.com>
2024-11-20 19:38:08 +0530

    Creating a middleware to decode user from a JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,12 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+var jwt = require("jsonwebtoken");
+var fetchUser = require("../middleware/fetchUser");
 
-const JWT_SECRET = "HarshisaGoodBOY";
+const JWT_SECRET = 'HarshisaGoodBOY';
 
+// MARK: 'ROUTE 1'
 // Create a User using : POST "/api/auth/createuser" . No login required
 router.post(
   "/createuser",
@@ -59,6 +61,7 @@ router.post(
   }
 );
 
+// MARK: 'ROUTE 2'
 // Authenticate a User using : POST "/api/auth/login" . No login required
 router.post(
   "/login",
@@ -103,4 +106,22 @@ router.post(
     }
   }
 );
+
+
+// MARK: 'ROUTE 3'
+// Get Loggedin User Deatils using : POST "/api/auth/getuser" . login required
+router.post(
+  "/getuser", fetchUser, async (req, res) => {
+
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password")
+      res.send(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  })
+
+
 module.exports = router;
